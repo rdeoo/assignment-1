3a3a0d8f66f5fb40f909e60891bc5357ee366e54
Rohan Deo <rohandeo307@gmail.com>
2023-04-07 17:13:54 -0700

    created two functions, darklighttoggle and changetext.DarkLightToggle changes both sides background colours and chnages the text colours to have good color contrast. i mader variables for everythiing i chnaaged above. changetext is a small function which only purpose is to change the text content in darktheme button to light theme when clicked and vice versa

---
+++ scripts/scripts.js
@@ -0,0 +1,35 @@
+const darkThemeButton = document.querySelector(".darktheme");
+const leftside = document.querySelector(".take-notes");
+const rideside = document.querySelector(".divclass");
+const h1text = document.querySelector(".mainh1");
+const footertext = document.querySelector(".pfooter");
+const aboutDocsText = document.querySelector(".aboutdoc");
+const textarea = document.querySelector("textarea");
+const h2text = document.querySelector("h2");
+const ulText = document.querySelector("ul");
+// const leftside = document.querySelector()
+
+function DarkLightToggle()
+{
+    leftside.classList.toggle("asidecolor");
+    rideside.classList.toggle("bodycolor");
+    h1text.classList.toggle("bodytext");
+    footertext.classList.toggle("bodytext");
+    aboutDocsText.classList.toggle("bodytext");
+    textarea.classList.toggle("textareaColor");
+    textarea.classList.toggle("asidetext");
+    h2text.classList.toggle("asidetext");
+    ulText.classList.toggle("asidetext");
+    
+
+}
+darkThemeButton.addEventListener("click", DarkLightToggle);
+
+function changeText()
+{
+    // if it says click me change to clicekd
+    // else change it to click me
+if(darkThemeButton.textContent === "Dark Theme"){darkThemeButton.textContent = "Light Theme"}
+else{darkThemeButton.textContent = "Dark Theme"}
+}
+darkThemeButton.addEventListener("click", changeText);
